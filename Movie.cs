0ee38dd781044b359650d2b6d3ecc4322aa5c6be
GratianBercuci <77671006+GratianBercuci@users.noreply.github.com>
2022-08-10 16:51:40 +0300

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MovieRentals
+{
+    public class Movie
+    {
+        public decimal Cost { get; set; }
+        public int Points { get; set; }
+        public string MovieType { get; set; }         
+        public string Title { get; set; }   
+        
+
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using MovieRentals;
+
+var movieList = new List<Movie>();
+movieList.Add(new Movie { Title = "\"500 days of Summer\"", Cost = 2, Points = 1, MovieType = "Regular" });
+movieList.Add(new Movie { Title = "\"Matilda\"", Cost = 1.5m, Points=1, MovieType = "Childrens" });
+movieList.Add(new Movie { Title = "\"Thor Love and Thunder\"", Cost = 3, Points = 2, MovieType = "New releases" });
+
+for (int i=0; i < movieList.Count; i++)
+{
+    Console.WriteLine("Title:" + movieList[i].Title + " cost:" + movieList[i].Cost + " points:" +movieList[i].Points);
+}
+
+
+
